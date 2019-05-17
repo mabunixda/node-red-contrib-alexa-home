49777308a11a4a757d7810d510b40c6c465dd964
Martin Buchleitner <mabunixda@gmail.com>
2019-05-17 12:08:06 +0200

    change test run to v10
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -u root --rm -it --network host -v $PWD:/src nodered/node-red-docker /bin/bash
+docker run -u root --rm -it --network host -v $PWD:/src nodered/node-red-docker:slim-v10 /bin/sh
