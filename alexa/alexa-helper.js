f05e24c9f5d1ed4506c544a56c668e564ce18c22
Martin Buchleitner <mabunixda@gmail.com>
2019-08-29 11:13:17 +0200

    introduce eslinting
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- alexa/alexa-helper.js
+++ alexa/alexa-helper.js
@@ -1,8 +1,16 @@
-"use strict";
+'use strict';
 
 module.exports = {
-    hubPort: process.env.ALEXA_PORT != undefined && parseInt(process.env.ALEXA_PORT) || 60000,
-    controllerNode: undefined,
-    isDebug: process.env.DEBUG && process.env.DEBUG.indexOf("node-red-contrib-alexa-home") > 0 || false,
-    bri_default: process.env.BRI_DEFAULT || 126,    
-}
+  hubPort: ( process.env.ALEXA_PORT != undefined
+             &&
+             parseInt(process.env.ALEXA_PORT)
+  )
+           || 60000,
+  controllerNode: undefined,
+  isDebug: ( process.env.DEBUG
+             &&
+             process.env.DEBUG.indexOf('node-red-contrib-alexa-home') > 0
+  )
+           || false,
+  bri_default: process.env.BRI_DEFAULT || 126,
+};
