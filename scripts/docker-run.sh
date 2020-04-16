4fa0cd2ecf558eedce81611595e41559df99f307
Martin Buchleitner <mabunixda@gmail.com>
2020-04-16 12:43:45 +0200

    ghaction: fix linter
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cd /src
+cd /src || exit 1
 npm install
 
 npm install -g npm-check
 npm install -g mocha
 npm install -g eslint
 
-cd /usr/src/node-red
+cd /usr/src/node-red || exit 2
 npm link /src
 
 /bin/sh
