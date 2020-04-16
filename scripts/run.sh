95a63c6679218afeca21d723f20317d54bdc2939
Martin Buchleitner <mabunixda@gmail.com>
2020-04-16 12:45:07 +0200

    ghaction: fix linter
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -u root --rm -it --network host -v $PWD:/src nodered/node-red-docker:slim-v10 /src/scripts/docker-run.sh
+docker run -u root --rm -it --network host -v "$PWD:/src" nodered/node-red-docker:slim-v10 /src/scripts/docker-run.sh
