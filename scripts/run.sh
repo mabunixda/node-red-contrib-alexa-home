c120dd3845193aedcc3c51bcf82b68f0073eca0b
Martin Buchleitner <mabunixda@gmail.com>
2021-10-08 14:21:21 +0200

    fix: master -> main

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-docker run -u root --rm -it \
-        --network host \
-        -v "$PWD:/src" \
-        --entrypoint /bin/sh \
-        nodered/node-red:latest-minimal \
-        /src/scripts/docker-run.sh
+cmd="podman"
+has_podman=$(hash docker 2> /dev/null)
+if [ "$?" == "0" ]; then
+    cmd="docker"
+fi
+
+$cmd run \
+    -u root \
+    --rm -it \
+    --network host \
+    --entrypoint /src/scripts/docker-run.sh \
+    -v "$PWD/:/src" \
+    docker.io/nodered/node-red:latest-minimal
