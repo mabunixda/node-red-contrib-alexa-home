ef2dad44ab794b3776645eac27053bbbb69ebda2
Martin Buchleitner <mabunixda@gmail.com>
2022-09-30 09:23:32 +0200

    fix: ci update

--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NODE_MAJOR=$(node --version | awk -F '.' '{print $1}')
-if [ "$NODE_MAJOR" = "v7" ] || [ "$NODE_MAJOR" = "v6" ]; then
-    exit 0
-fi
-
-./node_modules/eslint/bin/eslint.js alexa/*.js
